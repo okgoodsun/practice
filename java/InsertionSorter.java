b9324d0ce6f2df01ad2b66c140320305c7674e89
okgoodsun <okgoodsun@gmail.com>
2014-06-18 09:03:44 -0700

    Optimize insertion sort java implementation

--- java/InsertionSorter.java
+++ java/InsertionSorter.java
@@ -3,23 +3,17 @@ import java.util.Arrays;
 class InsertionSorter {
 
   public static int[] sort(int[] array) {
-    for (int i = 0, j = 0; i < array.length; i++) {
+    for (int i = 0; i < array.length; i++) {
       if (i < 1) continue; // first item, no need to compare
 
-      // j's value will be the first item smaller than i's value
-      for (j = i - 1; j >= 0; j--) {
-        if (array[i] >= array[j]) break;
-      }
-
-      // Insert array[i] to array[j+1] and move all items [j+1, i-1] to next postion
-      if (j < i - 1) {
-        int temp = array[i];
-
-        for (int k = i; k > j + 1; k--) {
-          array[k] = array[k - 1];
+      for (int j = i; j > 0; j--) {
+        if (array[j] < array[j - 1]) {
+          int x = array[j];
+          array[j] = array[j - 1];
+          array[j - 1] = x;
+        } else {
+          break;
         }
-        
-        array[j + 1] = temp;
       }
     }
 
